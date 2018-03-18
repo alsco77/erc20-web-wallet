6b1f380b33c2344fcb585a07b7bd019be5c03eb7
Alex Scott <alsco@Alexs-MacBook-Air.local>
2018-03-18 14:29:55 +1100

    Removed ganache dependency and used static ropsten url as web3 provider

--- src/app/core/web3.service.ts
+++ src/app/core/web3.service.ts
@@ -9,12 +9,10 @@ export class Web3Service {
   public web3: any;
 
   constructor() {
-    // if (typeof this.web3 !== 'undefined') {
-    //   this.web3 = new Web3(this.web3.currentProvider);
-    // } else {
-      const Ganache = require('ganache-core');
-      this.web3 = new Web3();
-      this.web3.setProvider(Ganache.provider());
-    // }
+    if (typeof this.web3 !== 'undefined') {
+      this.web3 = new Web3(this.web3.currentProvider);
+    } else {
+      this.web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'));
+    }
   }
 }
